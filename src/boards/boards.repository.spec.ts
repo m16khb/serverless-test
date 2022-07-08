0e6b74565d1e421735b33a84b23f5e75f2c731ee
김하빈 <m16khb@gmail.com>
2022-07-08 17:54:44 +0900

    test2

--- src/boards/boards.repository.spec.ts
+++
@@ -1,7 +0,0 @@
-import { BoardsRepository } from './boards.repository';
-
-describe('BoardsRepository', () => {
-  it('should be defined', () => {
-    expect(new BoardsRepository()).toBeDefined();
-  });
-});
